7e53ef3c8aa0897af043f21bdc4eb3faf8389ab8
Thomas Claude <claude.thomas@outlook.be>
2018-09-03 14:36:54 +0200

    :art: refactoring + adding article with comment pages

--- app/routes.php
+++ app/routes.php
@@ -2,6 +2,13 @@
 
 // Home page
 $app->get('/', function () use ($app) {
-    $articles = $app['dao.article']->findAll();
-    return $app['twig']->render('index.twig', array('articles' => $articles));
-});
+  $articles = $app['dao.article']->findAll();
+  return $app['twig']->render('index.twig', array('articles' => $articles));
+})->bind('home');
+
+// Article details with comments
+$app->get('/article/{id}', function ($id) use ($app) {
+  $article = $app['dao.article']->find($id);
+  $comments = $app['dao.comment']->findAllByArticle($id);
+  return $app['twig']->render('article.twig', array('article' => $article, 'comments' => $comments));
+})->bind('article');
